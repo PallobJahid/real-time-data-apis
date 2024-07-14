b991e08b168e3307e32946e71a2b8573dd725288
PallobST <pallobjahid.storexbd@gmail.com>
2024-07-14 18:43:45 +0600

    Change folder structures

---
+++ src/apis/long-polling.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fs = require('node:fs');
+const path = require('path');
+
+// Playground imports
+
+const PORT = 5001;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    let resolvedPath = path.resolve(__dirname, "..");
+    fs.readFile(resolvedPath + '/clients/long-poilling-client.html', 'utf8', (err, text) => {
+        res.send(text);
+    })
+})
+
+// Playground Implementations - Start
+// Playground Implementations - End
+
+
+app.listen(PORT, () => {
+    console.log(`App is listening at http://localhost:${PORT}`);
+})
